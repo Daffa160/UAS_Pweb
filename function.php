49834eac01b0b9a3fc08904fbec3e01b4304e3c5
Daffa Alif Murtaja <87127448+Daffa160@users.noreply.github.com>
2021-07-24 14:33:36 +0700

    fungsi post

--- function.php
+++ function.php
@@ -1,7 +1,18 @@
 <?php
 
 //koneksi ke database
-
 $conn = mysqli_connect("localhost","root","","UAS");
+
+
+if(isset($_POST['jual']))
+{
+    $idgame = $_POST['idgame'];
+    $namabarang = $_POST['namabarang'];
+    $level = $_POST['level'];
+    $harga = $_POST['harga'];
+
+    $addtotable = mysqli_query($conn, "insert into game (idgame, namabarang, level, harga) values('$idgame','$namabarang','$level','$harga')");
+    
+};
 
 ?>
