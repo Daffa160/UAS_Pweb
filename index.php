a52ff0f0993460f81400d84343e234609419ac98
Daffa Alif Murtaja <87127448+Daffa160@users.noreply.github.com>
2021-07-24 18:00:28 +0700

    Big Update

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
   require 'function.php';
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,57 +11,30 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Gaming Shop</title>
 </head>
-<body>
-<h1>Gaming Shop</h1>
+<body background="https://pbs.twimg.com/media/EL3RUttXsAA_pFA.jpg">
 <div class="navbar">
-    <a href="#home">Home</a>
-    <a href="#news">Beli</a>
-    <a href="#news">Jual</a>
+    <a href="index.php">Home Page</a>
     <div class="dropdown" >
-      <button class="dropbtn" >
+      <button class="dropbtn" >Beli
         <i class="fa fa-caret-down"></i>
       </button>
       <div class="dropdown-content">
-        <a href="#logout">Logout</a>
+        <a href="material.php">Material</a>
+        <a href="weapon.php">Weapon</a>
+        <a href="armor.php">Armor</a>
       </div>
     </div>
+    <a href="jual.php">Jual Item</a>
+    <a href="#logout" style="float: right;">Logout</i></a>
+    
 </div>     
     <br><br>
-    <table width="100%" cellspacing="0" border="1" align="center">
-        <thead>
-            <th>ID Penjual</th>
-            <th>Nama Item</th>
-            <th>Level</th>
-            <th>Harga</th>
-        </thead>
-        <?php
-          $ambilsemuadata = mysqli_query($conn, "select * from game");
-          while($data=mysqli_fetch_array($ambilsemuadata))
-          {
-            $idgame = $data['idgame'];
-            $namabarang = $data['namabarang'];
-            $level = $data['level'];
-            $harga = $data['harga'];
-            
-        ?>
-        <tr>
-          <td align="center"><?=$idgame;?></td>
-          <td align="center"><?=$namabarang;?></td>
-          <td align="center"><?=$level;?></td>
-          <td align="center"><?=$harga;?></td>
-        </tr>
-        <?php
-          };
-        ?>
-        <tfoot>
-            <th>ID Penjual</th>
-            <th>Nama Item</th>
-            <th>Level</th>
-            <th>Harga</th>
-        </tfoot>
-    </table>
-    <tbody>
-    </tbody>
-      
+    <center>
+      <h1 style="color: white;">Welcom to Gaming Shop</h1>
+      <hr style="margin:auto;width:40%">
+      <p style="color: white;">Buy Anything Or Sell Item You Want</p>
+      <p style="color: white;">With a High Price Or Lower Price</p>
+      <p style="color: white;">Whatever you are looking for will be found in the Gaming Shop</p>
+    </center>
 </body>
 </html>
